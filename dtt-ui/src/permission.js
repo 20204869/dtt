7a54ffb9a4b14238a335640047063fbdc912a701
zhangshanshan <Cango123>
2022-03-09 11:33:51 +0800

    data query

--- dtt-ui/src/permission.js
+++ dtt-ui/src/permission.js
@@ -71,8 +71,6 @@ router.beforeEach((to, from, next) => {
           router.push({ path: "/" }).catch(()=>{});
         })
       } else {
-         const fullPath = 'https://sso-test.cangoonline.net/?redirectUrl=http://localhost:8099&appId=appManager#/login'
-       // const fullPath = 'https://sso.cangoonline.net/?redirectUrl=http://datamanager.cangoonline.net/datamanager&appId=dataManager#/login'
         console.log(fullPath)
         next(false)
         window.location.href = fullPath
